3f37ce7f9f18280005f225671c0551557bf34364
Michael Dodwell <1372930+dodwmd@users.noreply.github.com>
2023-06-15 12:30:21 +1000

    fix substr

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_lambda_permission" "with_sns" {
 }
 
 resource "aws_lambda_function" "draining_lambda" {
-  function_name = format("%s-draining-function", var.autoscaling_group_name)
+  function_name = format("%s-draining-function", substr(var.autoscaling_group_name, 0, 45))
   role          = aws_iam_role.lambda.arn
   handler       = "index.lambda_handler"
   runtime       = "python3.7"
